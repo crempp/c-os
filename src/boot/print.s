a29ccaf882ef04a7eb3ba8eb8d997e1d230e1742
Chad Rempp <crempp@gmail.com>
2017-03-25 00:04:33 -0700

    Fake disk reads

--- src/boot/print.s
+++ src/boot/print.s
@@ -1,11 +1,11 @@
-; ------------------------------------------------------------------
+; -----------------------------------------------------------------------------
 ; Clears the screen to background
 ;
 ; PARAMETERS:
 ;   none
 ; RETURN:
 ;   none
-; ------------------------------------------------------------------
+; -----------------------------------------------------------------------------
 clear_screen:
     pusha               ; push general-purpose registers onto the stack
 
@@ -24,7 +24,7 @@ clear_screen:
     ret
 
 
-; ------------------------------------------------------------------
+; -----------------------------------------------------------------------------
 ; Moves cursor in text mode
 ;
 ; PARAMETERS:
@@ -32,7 +32,7 @@ clear_screen:
 ;   DL - Column
 ; RETURN:
 ;   none
-; ------------------------------------------------------------------
+; -----------------------------------------------------------------------------
 os_move_cursor:
     pusha
 
@@ -43,14 +43,15 @@ os_move_cursor:
     popa
     ret
 
-; ------------------------------------------------------------------
+
+; -----------------------------------------------------------------------------
 ; Print string currently pointed to by register BX
 ;
 ; PARAMETERS:
 ;   BX - Pointer to string to print
 ; RETURN:
 ;   none
-; ------------------------------------------------------------------
+; -----------------------------------------------------------------------------
 print:
     pusha                    ; push general-purpose registers onto the stack
 
@@ -81,7 +82,14 @@ print:
         ret                  ; return to calling IP
 
 
-
+; -----------------------------------------------------------------------------
+; Print newline
+;
+; PARAMETERS:
+;   none
+; RETURN:
+;   none
+; -----------------------------------------------------------------------------
 print_nl:
     pusha
 
@@ -95,50 +103,59 @@ print_nl:
     ret
 
 
+; -----------------------------------------------------------------------------
+; Print hexadecimal value
+;
+; Strategy: get the last char of 'dx', then convert to ASCII
+; Numeric ASCII values: '0' (ASCII 0x30) to '9' (0x39), so just add 0x30 to
+; byte N.
+; For alphabetic characters A-F: 'A' (ASCII 0x41) to 'F' (0x46) we'll add 0x40
+; Then, move the ASCII byte to the correct position on the resulting string
+;
+; PARAMETERS:
+;   DX - Hex value to print
+; RETURN:
+;   none
+; -----------------------------------------------------------------------------
 
 ; receiving the data in 'dx'
 ; For the examples we'll assume that we're called with dx=0x1234
 print_hex:
     pusha
 
-    mov cx, 0 ; our index variable
+    mov cx, 0                ; our index variable
 
-; Strategy: get the last char of 'dx', then convert to ASCII
-; Numeric ASCII values: '0' (ASCII 0x30) to '9' (0x39), so just add 0x30 to byte N.
-; For alphabetic characters A-F: 'A' (ASCII 0x41) to 'F' (0x46) we'll add 0x40
-; Then, move the ASCII byte to the correct position on the resulting string
 hex_loop:
-    cmp cx, 4 ; loop 4 times
+    cmp cx, 4                ; loop 4 times
     je end
 
     ; 1. convert last char of 'dx' to ascii
-    mov ax, dx ; we will use 'ax' as our working register
-    and ax, 0x000f ; 0x1234 -> 0x0004 by masking first three to zeros
-    add al, 0x30 ; add 0x30 to N to convert it to ASCII "N"
-    cmp al, 0x39 ; if > 9, add extra 8 to represent 'A' to 'F'
+    mov ax, dx               ; we will use 'ax' as our working register
+    and ax, 0x000f           ; 0x1234 -> 0x0004 by masking first three to zeros
+    add al, 0x30             ; add 0x30 to N to convert it to ASCII "N"
+    cmp al, 0x39             ; if > 9, add extra 8 to represent 'A' to 'F'
     jle step2
-    add al, 7 ; 'A' is ASCII 65 instead of 58, so 65-58=7
+    add al, 7                ; 'A' is ASCII 65 instead of 58, so 65-58=7
 
 step2:
     ; 2. get the correct position of the string to place our ASCII char
     ; bx <- base address + string length - index of char
-    mov bx, HEX_OUT + 5 ; base + length
-    sub bx, cx  ; our index variable
-    mov [bx], al ; copy the ASCII char on 'al' to the position pointed by 'bx'
-    ror dx, 4 ; 0x1234 -> 0x4123 -> 0x3412 -> 0x2341 -> 0x1234
+    mov bx, HEX_OUT + 5      ; base + length
+    sub bx, cx               ; our index variable
+    mov [bx], al             ; copy the ASCII char on 'al' to the position
+                             ; pointed by 'bx'
+    ror dx, 4                ; 0x1234 -> 0x4123 -> 0x3412 -> 0x2341 -> 0x1234
 
-    ; increment index and loop
-    add cx, 1
-    jmp hex_loop
+    add cx, 1                ; increment index
+    jmp hex_loop             ; and loop
 
 end:
-    ; prepare the parameter and call the function
-    ; remember that print receives parameters in 'bx'
-    mov bx, HEX_OUT
-    call print
+
+    mov bx, HEX_OUT          ; prepare the parameter
+    call print               ; and call the print function
 
     popa
     ret
 
 HEX_OUT:
-    db '0x0000',0 ; reserve memory for our new string
+    db '0x0000',0            ; reserve memory for our new string
